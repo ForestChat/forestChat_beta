595e8631ca875c0503650e954f24d49807fa00e8
Matthieu Bouquet <matthieu_bouquet@hotmail.fr>
2021-04-10 20:47:37 +0200

    rename packages

---
+++ presentation/src/main/java/com/forest/forestchat/injection/AppComponent.kt
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2017 Moez Bhatti <moez.bhatti@gmail.com>
+ *
+ * This file is part of QKSMS.
+ *
+ * QKSMS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * QKSMS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with QKSMS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.forest.forestchat.injection
+
+import com.forest.forestchat.common.QKApplication
+import com.forest.forestchat.common.QkDialog
+import com.forest.forestchat.common.util.QkChooserTargetService
+import com.forest.forestchat.common.widget.AvatarView
+import com.forest.forestchat.common.widget.PagerTitleView
+import com.forest.forestchat.common.widget.PreferenceView
+import com.forest.forestchat.common.widget.QkEditText
+import com.forest.forestchat.common.widget.QkSwitch
+import com.forest.forestchat.common.widget.QkTextView
+import com.forest.forestchat.common.widget.RadioPreferenceView
+import com.forest.forestchat.feature.backup.BackupController
+import com.forest.forestchat.feature.blocking.BlockingController
+import com.forest.forestchat.feature.blocking.manager.BlockingManagerController
+import com.forest.forestchat.feature.blocking.messages.BlockedMessagesController
+import com.forest.forestchat.feature.blocking.numbers.BlockedNumbersController
+import com.forest.forestchat.feature.compose.editing.DetailedChipView
+import com.forest.forestchat.feature.conversationinfo.injection.ConversationInfoComponent
+import com.forest.forestchat.feature.settings.SettingsController
+import com.forest.forestchat.feature.settings.about.AboutController
+import com.forest.forestchat.feature.settings.swipe.SwipeActionsController
+import com.forest.forestchat.feature.themepicker.injection.ThemePickerComponent
+import com.forest.forestchat.feature.widget.WidgetAdapter
+import com.forest.forestchat.injection.android.ActivityBuilderModule
+import com.forest.forestchat.injection.android.BroadcastReceiverBuilderModule
+import com.forest.forestchat.injection.android.ServiceBuilderModule
+import dagger.Component
+import dagger.android.support.AndroidSupportInjectionModule
+import javax.inject.Singleton
+
+@Singleton
+@Component(modules = [
+    AndroidSupportInjectionModule::class,
+    AppModule::class,
+    ActivityBuilderModule::class,
+    BroadcastReceiverBuilderModule::class,
+    ServiceBuilderModule::class])
+interface AppComponent {
+
+    fun conversationInfoBuilder(): ConversationInfoComponent.Builder
+    fun themePickerBuilder(): ThemePickerComponent.Builder
+
+    fun inject(application: QKApplication)
+
+    fun inject(controller: AboutController)
+    fun inject(controller: BackupController)
+    fun inject(controller: BlockedMessagesController)
+    fun inject(controller: BlockedNumbersController)
+    fun inject(controller: BlockingController)
+    fun inject(controller: BlockingManagerController)
+    fun inject(controller: SettingsController)
+    fun inject(controller: SwipeActionsController)
+
+    fun inject(dialog: QkDialog)
+
+    fun inject(service: WidgetAdapter)
+
+    /**
+     * This can't use AndroidInjection, or else it will crash on pre-marshmallow devices
+     */
+    fun inject(service: QkChooserTargetService)
+
+    fun inject(view: AvatarView)
+    fun inject(view: DetailedChipView)
+    fun inject(view: PagerTitleView)
+    fun inject(view: PreferenceView)
+    fun inject(view: RadioPreferenceView)
+    fun inject(view: QkEditText)
+    fun inject(view: QkSwitch)
+    fun inject(view: QkTextView)
+
+}
---
+++ presentation/src/main/java/com/forest/forestchat/injection/android/ActivityBuilderModule.kt
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 2017 Moez Bhatti <moez.bhatti@gmail.com>
+ *
+ * This file is part of QKSMS.
+ *
+ * QKSMS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * QKSMS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with QKSMS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.forest.forestchat.injection.android
+
+import com.forest.forestchat.feature.backup.BackupActivity
+import com.forest.forestchat.feature.blocking.BlockingActivity
+import com.forest.forestchat.feature.compose.ComposeActivity
+import com.forest.forestchat.feature.compose.ComposeActivityModule
+import com.forest.forestchat.feature.contacts.ContactsActivity
+import com.forest.forestchat.feature.contacts.ContactsActivityModule
+import com.forest.forestchat.feature.conversationinfo.ConversationInfoActivity
+import com.forest.forestchat.feature.gallery.GalleryActivity
+import com.forest.forestchat.feature.gallery.GalleryActivityModule
+import com.forest.forestchat.feature.main.MainActivity
+import com.forest.forestchat.feature.main.MainActivityModule
+import com.forest.forestchat.feature.notificationprefs.NotificationPrefsActivity
+import com.forest.forestchat.feature.notificationprefs.NotificationPrefsActivityModule
+import com.forest.forestchat.feature.plus.PlusActivity
+import com.forest.forestchat.feature.plus.PlusActivityModule
+import com.forest.forestchat.feature.qkreply.QkReplyActivity
+import com.forest.forestchat.feature.qkreply.QkReplyActivityModule
+import com.forest.forestchat.feature.scheduled.ScheduledActivity
+import com.forest.forestchat.feature.scheduled.ScheduledActivityModule
+import com.forest.forestchat.feature.settings.SettingsActivity
+import com.forest.forestchat.injection.scope.ActivityScope
+import dagger.Module
+import dagger.android.ContributesAndroidInjector
+
+@Module
+abstract class ActivityBuilderModule {
+
+    @ActivityScope
+    @ContributesAndroidInjector(modules = [MainActivityModule::class])
+    abstract fun bindMainActivity(): MainActivity
+
+    @ActivityScope
+    @ContributesAndroidInjector(modules = [PlusActivityModule::class])
+    abstract fun bindPlusActivity(): PlusActivity
+
+    @ActivityScope
+    @ContributesAndroidInjector(modules = [])
+    abstract fun bindBackupActivity(): BackupActivity
+
+    @ActivityScope
+    @ContributesAndroidInjector(modules = [ComposeActivityModule::class])
+    abstract fun bindComposeActivity(): ComposeActivity
+
+    @ActivityScope
+    @ContributesAndroidInjector(modules = [ContactsActivityModule::class])
+    abstract fun bindContactsActivity(): ContactsActivity
+
+    @ActivityScope
+    @ContributesAndroidInjector(modules = [])
+    abstract fun bindConversationInfoActivity(): ConversationInfoActivity
+
+    @ActivityScope
+    @ContributesAndroidInjector(modules = [GalleryActivityModule::class])
+    abstract fun bindGalleryActivity(): GalleryActivity
+
+    @ActivityScope
+    @ContributesAndroidInjector(modules = [NotificationPrefsActivityModule::class])
+    abstract fun bindNotificationPrefsActivity(): NotificationPrefsActivity
+
+    @ActivityScope
+    @ContributesAndroidInjector(modules = [QkReplyActivityModule::class])
+    abstract fun bindQkReplyActivity(): QkReplyActivity
+
+    @ActivityScope
+    @ContributesAndroidInjector(modules = [ScheduledActivityModule::class])
+    abstract fun bindScheduledActivity(): ScheduledActivity
+
+    @ActivityScope
+    @ContributesAndroidInjector(modules = [])
+    abstract fun bindSettingsActivity(): SettingsActivity
+
+    @ActivityScope
+    @ContributesAndroidInjector(modules = [])
+    abstract fun bindBlockingActivity(): BlockingActivity
+
+}
